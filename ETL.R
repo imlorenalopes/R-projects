bf6345715eab3d30c365dd4551958e3fdaae4e44
imlorenalopes <lorenalopes@usp.br>
2025-01-10 23:21:33 -0300

    adiçaõ de linhas ao código

---
+++ ETL.R
@@ -0,0 +1,8 @@
+# importação das bibliotecas ----------------------------------------------
+
+library(tidyverse)
+library(readxl)
+
+# Adequação do ambiente ---------------------------------------------------
+options(scipen = 999)
+rm(list = ls())
